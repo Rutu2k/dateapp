5961364c374d5548e69424ba34947cabc44fe503
Rutu <rutudhokchaule@gmail.com>
2020-06-04 20:06:45 +0530

    Added modal to Buy button

--- src/components/WatchComponent.js
+++ src/components/WatchComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Media, Button,} from 'reactstrap';
+import { Media, Button, Modal, ModalBody, ModalHeader,} from 'reactstrap';
 
 const imgStyle = {
     maxHeight: 300,
@@ -8,8 +8,17 @@ const imgStyle = {
 class Menu extends Component {
     constructor(props) {
         super(props);
-    }
 
+        this.state= {
+            isBuyOpen :false
+        };
+        this.toggleBuy = this.toggleBuy.bind(this);
+    }
+    toggleBuy() {
+        this.setState({
+            isBuyOpen: !this.state.isBuyOpen
+        });
+    }
     render() {
     const menu = this.props.dishes.map((dish) => {
         return (
@@ -23,16 +32,24 @@ class Menu extends Component {
                     <Media heading>{dish.name}</Media>
                     <p>{dish.description}</p>
                     <h5 >Rs.{dish.price}</h5>
-                    <Button onClick= {this.toggleComment}>Read More</Button>
+                    <Button outline >Read More</Button>
                 </Media>
 
                 <Media right middle className="sm mt-5">
                     <Button className="btn"><span className="fa fa-bookmark fa"></span> Add to Cart</Button>
                     <br></br>
                     <br></br>
-                    <Button className="btn"><span className="fa fa-tag fa"></span> Buy Item</Button>
+                    <Button className="btn" onClick= {this.toggleBuy}><span className="fa fa-tag fa"></span> Buy Item</Button>
                 </Media>
             </Media>
+            <Modal isOpen={this.state.isBuyOpen} toggle={this.toggleBuy}>
+                <ModalHeader>
+                    Buy Item
+                </ModalHeader>
+                <ModalBody>
+                    <h5>Our Precious Customer,<br></br> We are really sorry for your inconvenience! Our online payment feature is under construction right now! So stay tuned,and we appreciate your time!<br></br> Thank you! </h5>
+                </ModalBody>
+            </Modal>
             </div>
         );
     });
